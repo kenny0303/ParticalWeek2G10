8d9e7c4e7a9425154038617d6df1a078b352df10
anreh <anurehka@gmail.com>
2023-07-24 09:02:17 +0800

    cgj

---
+++ app/src/main/java/com/example/particalweek2g10/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.particalweek2g10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val doneBtn : Button = findViewById(R.id.done_button)
+
+        doneBtn.setOnClickListener{
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+    }
+}
